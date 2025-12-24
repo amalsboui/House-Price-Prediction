1951b22b20079be43723a39a82ba0a8a3c071a4d
Amal Sboui <amal.sboui@insat.ucar.tn>
2025-12-24 14:35:51 +0100

    immobilier scraper working

--- housescraper/housescraper/pipelines.py
+++ housescraper/housescraper/pipelines.py
@@ -1,13 +1,71 @@
-# Define your item pipelines here
+# Define your adapter pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# See: https://docs.scrapy.org/en/latest/topics/adapter-pipeline.html
 
 
-# useful for handling different item types with a single interface
+# useful for handling different adapter types with a single interface
+
+import re
+from scrapy.exceptions import DropItem
 from itemadapter import ItemAdapter
 
 
 class HousescraperPipeline:
+
+    allowed_types = ["Villa", "Appart", "Duplex", "Triplex"]
+
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+        
+        # Clean titre
+        if adapter.get("titre"):
+            adapter["titre"] = adapter["titre"].strip()
+
+        # Clean prix
+        if adapter.get("prix"):
+            prix = adapter["prix"].replace("DT", "").replace(" ", "").strip()
+            try:
+                adapter["prix"] = float(prix)
+            except ValueError:
+                adapter["prix"] = None
+        
+        # Clean surface
+        if adapter.get("surface"):
+            surface = adapter["surface"].replace("m", "").strip()
+            try:
+                adapter["surface"] = float(surface)
+            except ValueError:
+                adapter["surface"] = None
+
+        # Clean ville
+        if adapter.get("ville"):
+            adapter["ville"] = adapter["ville"].strip()
+
+        # Clean chambres
+        if adapter.get("chambres"):
+            try:
+                adapter["chambres"] = int(adapter["chambres"].split()[0])
+            except ValueError:
+                adapter["chambres"] = None
+
+        if adapter.get("chambres") is None:
+            raise DropItem(f"No chambre info for {adapter.get('titre')}")
+        
+        # Clean type_bien
+        if adapter.get("type_bien"):
+            adapter["type_bien"] = adapter["type_bien"].strip()
+
+        # Drop items with missing fields
+        essential_fields = ["chambres", "type_bien", "surface", "prix", "ville", "titre"]
+        for f in essential_fields:
+            if adapter.get(f) is None:
+                raise DropItem(f"Missing {f} for item: {adapter.get('titre')}")
+        
+        # Drop items with type_bien not allowed
+        if adapter.get("type_bien") not in self.allowed_types:
+            raise DropItem(f"Ignored adapter because type_bien not allowed: {adapter.get('type_bien')}")
+
+
         return item
--- housescraper/housescraper/spiders/immobilierspider.py
+++ housescraper/housescraper/spiders/immobilierspider.py
@@ -3,6 +3,8 @@ from housescraper.items import HouseItem
 
 
 
+
+
 class ImmobilierspiderSpider(scrapy.Spider):
     name = "immobilierspider"
     allowed_domains = ["www.immobilier.com.tn"]
@@ -16,16 +18,16 @@ class ImmobilierspiderSpider(scrapy.Spider):
 
             house_item["titre" ] = house.css("h3::text").get()
             house_item["prix"] = house.css("div.price span::text").get()
-            house_item["surface"] = house.css("ul.amenities li:first-child::text").get()
-            house_item["chambres"]= house.css("ul.amenities li.chambres::text").get()
             house_item["ville"] = house.css("small::text").get()
-            house_item["type_bien"] = house.css("ul.amenities li:nth-child(2)::text").get()
+            house_item["surface"] = house.xpath(".//i[contains(@class, 'icon-area')]/following-sibling::text()").get()
+            house_item["chambres"] = house.xpath(".//i[contains(@class, 'icon-bedrooms')]/following-sibling::text()").get()
+            house_item["type_bien"] = house.css("ul.amenities li:last-child::text").get()
 
             yield house_item
 
-        next_page = response.css("a.next::attr(href)").get()
+        next_page = response.css("a[aria-label='Page suivante']::attr(href)").get()
         if next_page:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, self.parse)
 
 
 
